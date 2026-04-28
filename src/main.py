788ed6d38a630fdb3cc54164a7966692669a7dee
WasifJavaBS1357 <wasif.islam@Brainstation-23.com>
2026-04-28 23:12:17 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,3 +1,11 @@
 import datetime
+from utils import add, subtract
+
 print("My name is wasif")
 print(f"Today's date is {datetime.date.today()}")
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print(f"Addition: {add(a, b)}")
+print(f"Subtraction: {subtract(a, b)}")
