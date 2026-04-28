58b64f0a7da1a0c8e7efc16000a2fac1df5025b5
unoxidized-copper <h.tarik920@gmail.com>
2026-04-28 23:48:36 +0600

    Add multiplication function

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import datetime
-from utils import add, subtract
+from utils import add, subtract, multiply
 
 print("My name is wasif")
 print(f"Today's date is {datetime.date.today()}")
@@ -9,3 +9,4 @@ b = int(input("Enter second number: "))
 
 print(f"Addition: {add(a, b)}")
 print(f"Subtraction: {subtract(a, b)}")
+print(f"Multiplication: {multiply(a, b)}")
